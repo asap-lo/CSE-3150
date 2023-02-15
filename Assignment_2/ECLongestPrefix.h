be9e38ff23d5e6f8138c3038673238fe628ba21a
asap-lo <jj.lombardo02@gmail.com>
2023-02-15 08:39:40 -0500

    removed Assignments to move them

--- Assignment_2/ECLongestPrefix.h
+++
@@ -1,8 +0,0 @@
-#ifndef EC_LONGEST_PREFIX_H
-#define EC_LONGEST_PREFIX_H
-
-#include <string>
-
-std::string ECLongestPrefix(int numStrings, const std::string arrayStrings[]);
-
-#endif // EC_LONGEST_PREFIX_H

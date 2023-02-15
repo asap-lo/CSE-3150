15dca4fdb866d6890bea8352491beaecd5fe61cd
asap-lo <jj.lombardo02@gmail.com>
2023-02-15 08:19:45 -0500

    Updated file

--- Assignment_2/ECConsecutiveInts.cpp
+++ Assignment_2/ECConsecutiveInts.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 // Test whether strInput is a string composed of consecutive and increasing integers (in decimal formats)
@@ -25,6 +26,52 @@ using namespace std;
 bool ECConsecutiveInts(const string &strInput)
 {
   // your code  here
-  cout << "hello" << endl;
-}
+  int index = 0;
+  int num;
+  int next;
+  int digits = 1;
 
+  while (1)
+  {
+    // check to see if the strInput has enough digits left to support next, if not, and digits >= 6, return false
+    // try/catch?
+    // Having issue with 9->10 case, when should I get number of digits?
+    //910111213
+    try
+    {
+      num = stoi(strInput.substr(index, digits));
+      next = num + 1;
+      int nextDigits = int(log10(next) + 1);
+      if (next == stoi(strInput.substr(index + digits, nextDigits)))
+      {
+        cout << num << endl;
+        cout << next << endl;
+        index = index + digits;
+        if (index + digits == strInput.length())
+        {
+          return true;
+        }
+        digits = nextDigits;
+      }
+      else
+      {
+        index = 0;
+        digits += 1;
+        continue;
+      }
+    }
+    catch (const std::exception &e)
+    {
+      // std::cerr << e.what() << '\n';
+      if (digits >= 6)
+      {
+        return false;
+      }
+      index = 0;
+      digits += 1;
+    }
+
+    
+  }
+  return true;
+}

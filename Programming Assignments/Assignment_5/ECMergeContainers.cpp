c8864f0c5649db340fa4205474eb2b36027aa53a
John Lombardo <john.j.lombardo@uconn.edu>
2023-02-20 17:23:56 -0500

    Assignment 5 added

---
+++ Programming Assignments/Assignment_5/ECMergeContainers.cpp
@@ -0,0 +1,8 @@
+// Created by Y. Wu, Jan 31, 2023
+// Merge lists (of any STL container type) into a single sorted list (of any STL container type)
+// Duplicates allowed unless the output doesn't take duplicates (e.g., a map)
+// Note: you should make your code as flexible as possible: you should allow all kinds of outputs: vector, set, etc 
+// Make your code short: at most 10 lines of code (LOC) 
+
+// now defiine ECMergeContainers ...
+

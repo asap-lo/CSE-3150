1827d0d4845f9c97f4db8362dea71b781eebbd13
asap-lo <jj.lombardo02@gmail.com>
2023-03-02 15:13:48 -0500

    A6 added

---
+++ Programming Assignments/Assignment_6/ECSimTask2.cpp
@@ -0,0 +1,29 @@
+//
+//  ECSimTask2.cpp
+//
+//
+//
+
+#include "ECSimTask2.h"
+
+ECMultiIntervalsTask::ECMultiIntervalsTask(const std::string &tid) : ECSimTask(tid)
+{
+}
+
+void ECMultiIntervalsTask::AddInterval(int tmStart, int tmEnd)
+{
+    tmStarts.push_back(tmStart);
+    tmEnds.push_back(tmEnd);
+}
+
+bool ECMultiIntervalsTask::IsReadyToRun(int tick) const
+{
+    for (int i = 0; i < tmStarts.size(); i++)
+    {
+        if (tick >= tmStarts[i] && tick <= tmEnds[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
